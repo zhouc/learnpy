1915e8631ebe1a5cab756b16b73bcfe2789c5158
unknown <zhouchao1@360buyAD.local>
2016-09-12 20:38:13 +0800

    update ex8

--- py_file/ex8.py
+++ py_file/ex8.py
@@ -3,7 +3,7 @@ formatter = "%r %r %r %r"
 print formatter % (1, 2, 3, 4)
 print formatter % ("one", "two", "three", "four")
 print formatter % (True, False, False, True)
-print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (forematter, formatter, formatter, formatter)
 print formatter % (
     "I had this thing.",
     "That you could type up right.",
