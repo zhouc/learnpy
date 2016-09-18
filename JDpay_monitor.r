04c741aa5c0a188bb7413f6c7c1df065cd6da655
zhouchao <zhouchaowow1@sina.com>
2016-09-18 22:41:44 +0800

    update jdpayfile

--- JDpay_monitor.r
+++ JDpay_monitor.r
@@ -1,50 +1,81 @@
-#瀵煎叆鏀粯鏁版嵁
-data_pay<-read.csv("D:\\宸ヤ綔鏂囦欢\\澶栭儴鏀粯鍦烘櫙椋庢帶\\澶栭儴鏀粯鎷︽埅鐩戞帶\\paydatafile.csv")
-#澶勭悊琛ㄥご
+#导入支付数据
+data_pay<-read.csv("E:\\learnpy\\paydatafile.csv")
+#处理表头
 colnum<-ncol(data_pay)
 for(i in 1:colnum){
-        colnames(data_pay)[i]<- unlist(strsplit(colnames(data_pay)[i],".",fixed = TRUE))[2]
+  colnames(data_pay)[i]<- unlist(strsplit(colnames(data_pay)[i],".",fixed = TRUE))[2]
 }
-#浜笢鏀粯
+#京东支付
 library(dplyr)
 jd_pay<- data_pay %>% tbl_df() %>% 
-        mutate(date = as.Date(paste(substr(dt,1,4),"/",substr(dt,5,6),"/",substr(dt,7,8),sep =""))) %>%
-        mutate(type = ifelse(code == 0,'閫氳繃',ifelse(code == 80000,'鍔犻獙',ifelse(code == 90000,'鎷︽埅','鍏朵粬')))) %>%
-        mutate(source = ifelse(sourcetype == 'BT-WAIDAN-JDPAY','浜笢鏀粯',ifelse(sourcetype == 'BT-SHANFU','閾惰仈闂粯',ifelse(sourcetype == 'BT-WAIDAN-QB','閽卞寘鏀粯',ifelse(sourcetype == 'BT-DJ-JDPAY','浜笢鍒板','鍏朵粬'))))) %>%
-        select(date,source,type,pin_num,hit_num) %>% 
-        arrange(source,type)
-library(reshape2)
-jd_pay_0<- jd_pay %>% filter(source =='浜笢鏀粯') %>% select (-hit_num)
+  mutate(date = as.Date(paste(substr(dt,1,4),"/",substr(dt,5,6),"/",substr(dt,7,8),sep =""))) %>%
+  mutate(type = ifelse(code == 0,'通过',ifelse(code == 80000,'加验',ifelse(code == 90000,'拦截','其他')))) %>%
+  mutate(source = ifelse(sourcetype == 'BT-WAIDAN-JDPAY','京东支付',ifelse(sourcetype == 'BT-SHANFU','银联闪付',ifelse(sourcetype == 'BT-WAIDAN-QB','钱包支付',ifelse(sourcetype == 'BT-DJ-JDPAY','京东到家','其他'))))) %>%
+  select(date,source,type,pin_num,hit_num) %>% 
+  arrange(source,type)
+library(reshape)
+jd_pay_0<- jd_pay %>%  select (-hit_num)
 jdpay_rate_01<-melt(as.data.frame(jd_pay_0),id = c('date','source','type'))
 jdpay_rate_02<-cast(jdpay_rate_01,date + source ~ type,sum) %>% 
         tbl_df() %>%        
-        mutate(verify_rate = 鍔犻獙 /(鍔犻獙+閫氳繃))
-#浣跨敤ggplot2鐢诲浘
+        mutate(verify_rate = 加验 /(加验+通过))
+#使用ggplot2画图
 library(ggplot2)
-ggplot(jd_pay,aes(x=date,y=pin_num,fill = type)) + geom_bar(stat = "identity")+
-        ggtitle("鏀粯") +
+library(plyr)
+jd_pay_001<-ddply(jd_pay,.(date,source),transform,label_y=cumsum(pin_num))
+jd1<-ggplot(jd_pay_001,aes(x=date,y=pin_num,fill = type)) + 
+        geom_bar(stat = "identity")+
+        ggtitle("天盾安全_外部支付") +
+        geom_text(aes(y=label_y,label =pin_num),vjust = 1.5,color = "white",size =3)+
         theme(axis.text.x= element_text(size = 6,colour = "black"),
               axis.text.y= element_text(size = 6,colour = "black"),
               legend.text =element_text(size = 6))+
-        facet_grid(source ~ .)+
-        xlab("鏃ユ湡")+
-        ylab("鏁伴噺")
+        facet_grid(. ~ source)+
+        xlab("日期")+
+        ylab("数量")
 
-#浣跨敤plotly鐢诲浘
+jd2<-ggplot(jdpay_rate_02,aes(x=date,y=verify_rate)) + 
+        geom_line()+
+        theme(axis.text.x= element_text(size = 6,colour = "black"),
+              axis.text.y= element_text(size = 6,colour = "black"),
+              legend.text =element_text(size = 6))+
+        facet_grid(. ~ source)+
+        xlab("日期")+
+        ylab("加验率")
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+library(grid)
+pdf("JD_PAY_verify.pdf",width = 16,height = 12)
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(2,2)))
+vplayout<-function(x,y)
+        viewport(layout.pos.row = x,layout.pos.col = y)
+print(jd1,vp = vplayout(1,1:2))
+print(jd2,vp = vplayout(2,1:2))
+dev.off()
+
+
+
+
+
+
+
+
+
+#使用plotly画图
 library(plotly)
 m = list(
-        l = 50,
-        r = 50,
-        b = 100,
-        t = 100,
-        pad = 4
+  l = 50,
+  r = 50,
+  b = 100,
+  t = 100,
+  pad = 4
 )
 
 subplot(
-        plot_ly(jd_pay %>% filter(source == "浜笢鏀粯"),x = date,y = pin_num,color = type,type ="bar") %>% 
-                layout(barmode = "stack", width = 1200, height = 500, margin = m),
-        plot_ly(jdpay_rate_02,x=date,y=verify_rate,line = list(shape = "spline")),
-        margin = 0.05
+  plot_ly(jd_pay %>% filter(source == "京东支付"),x = date,y = pin_num,color = type,type ="bar") %>% 
+    layout(barmode = "stack", width = 1200, height = 500, margin = m),
+  plot_ly(jdpay_rate_02,x=date,y=verify_rate,line = list(shape = "spline")),
+  margin = 0.05
 ) %>% layout(showlegend = FALSE)
 
 
